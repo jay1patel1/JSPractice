4859e0afee86f1a44a7e8c5cee988e18ed6d9d0d
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2023-12-12 19:56:55 +0530

    updated

--- Recursion Practice/Fibonacci.js
+++ Recursion Practice/Fibonacci.js
@@ -1,11 +1,10 @@
 function fibonacci(n) {
+    console.log(n)
     if (n === 1) {
         return 1;
     } else {
-        let retunValue = (fibonacci(n)-1) + (fibonacci(n)-2);
-        return retunValue;
+        return fibonacci(n-1) + fibonacci (n-2);
     }
-    console.log(n)
 }
 
 fibonacci(7);
