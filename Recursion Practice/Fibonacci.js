541a650dafd4cd0d3b83a65998bc938b22fec2f5
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2023-12-12 20:14:14 +0530

    updated

--- Recursion Practice/Fibonacci.js
+++ Recursion Practice/Fibonacci.js
@@ -1,10 +1,39 @@
 function fibonacci(n) {
-    console.log(n)
-    if (n === 1) {
-        return 1;
-    } else {
-        return fibonacci(n-1) + fibonacci (n-2);
+    let value = n;
+
+    // Value sanity check
+    if (typeof value !== "number") {
+        let convertedValue = parseInt(value);
+        if (isNaN(convertedValue)) {
+            console.log("The entered value is not a number");
+            return;
+        } else {
+            value = convertedValue;
+        }
     }
+
+    // Check it is positive value
+    if (value < 0) {
+        console.log("The entered Value is smaller than zero");
+        return;
+    }
+
+    // Fibonacci Calculator Function
+    function fibonacciCalc(x) {
+        if ( x <= 1) {
+            return x;
+        } else {
+            return (fibonacciCalc (x-1) + fibonacciCalc(x-2))
+        }
+    }
+
+    // Return Code 
+    console.log(fibonacciCalc (value));
 }
 
-fibonacci(7);
+fibonacci(10);
+// Result: 55
+fibonacci(2);
+// Result: 1
+fibonacci(-10);
+// Result: The entered Value is smaller than zero
